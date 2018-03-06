d27ecd8f3b023cb8e9b141e8d088b33e04f6937c
Dibb, Jim <jim.dibb@emc.com>
2018-03-06 11:56:53 -0500

    added multithreading to to mannnny iterations

--- puzzle/main.go
+++ puzzle/main.go
@@ -5,35 +5,45 @@ import "math/rand"
 
 var odds, evens float32
 
-func main() {
-	rand.Seed(9825245)
-	checkmap := make(map[int]int)
+func compute(lim int, c chan int) {
 	tickets := [18]int{3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1}
-	for i := 0; i < 200000; i++ {
+	var odds int
+	for i := 0; i < lim; i++ {
 		tmap := make(map[int]bool)
 
 		sum := 0
 		for n := 0; n < 3; n++ {
 			for true {
 				tick := rand.Intn(18)
-				checkmap[tick]++
+
 				if tmap[tick] == false {
 					tmap[tick] = true
 					sum += tickets[tick]
-
 					break
 				}
 			}
 		}
-		fmt.Println(sum)
-		if sum%2 != 0 {
+		if sum%2 == 1 {
 			odds++
-		} else {
-			evens++
 		}
-
 	}
-	fmt.Print(checkmap)
-	p := odds / (odds + evens)
-	fmt.Printf("Odds: %f, Evens: %f , Prob %f \n", odds, evens, p)
+	c <- odds
+}
+
+func main() {
+	rand.Seed(9825245)
+	limit := 200000
+	procs := 1000
+	c := make(chan int)
+	for i := 0; i < procs; i++ {
+		go compute(limit, c)
+	}
+
+	var odds int64
+	for i := 0; i < procs; i++ {
+		odds += int64(<-c)
+	}
+	evens := int64(procs*limit) - odds
+
+	fmt.Printf("Odds: %d, Evens: %d", odds, evens)
 }
