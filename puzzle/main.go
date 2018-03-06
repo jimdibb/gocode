fe6f7b87f594107a43bc0487d088708c4e2e3690
Dibb, Jim <jim.dibb@emc.com>
2018-03-06 13:49:12 -0500

    figured out what with replacement means

--- puzzle/main.go
+++ puzzle/main.go
@@ -9,19 +9,21 @@ func compute(lim int, c chan int) {
 	tickets := [18]int{3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1}
 	var odds int
 	for i := 0; i < lim; i++ {
-		tmap := make(map[int]bool)
+		//	tmap := make(map[int]bool)
 
 		sum := 0
 		for n := 0; n < 3; n++ {
-			for true {
-				tick := rand.Intn(18)
 
+			tick := rand.Intn(18)
+			/*
 				if tmap[tick] == false {
 					tmap[tick] = true
 					sum += tickets[tick]
 					break
 				}
-			}
+			*/
+			sum += tickets[tick]
+
 		}
 		if sum%2 == 1 {
 			odds++
